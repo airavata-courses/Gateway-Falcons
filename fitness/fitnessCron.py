0a06c271b51ff003b999308fba11f8e8a07fa3d6
Praneta Paithankar <ppaithan@iu.edu>
2019-02-09 21:16:12 -0500

    Change time to 5 minutes for peer review

--- fitness/fitnessCron.py
+++ fitness/fitnessCron.py
@@ -4,6 +4,6 @@ from crontab import CronTab
 cron = CronTab(user=True)
 job = cron.new(command='curl -v http://127.0.0.1:5001/get',comment='add fitness data')
 
-job.setall('59 8-23/1 * * *')
+job.setall('05 * * * *')
 
 cron.write()
