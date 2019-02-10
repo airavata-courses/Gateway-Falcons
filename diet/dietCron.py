42250242f24bc5a946a4c97cbc9e3af9421370d0
Praneta Paithankar <ppaithan@iu.edu>
2019-02-09 21:17:29 -0500

    Update dietCron.py

--- diet/dietCron.py
+++ diet/dietCron.py
@@ -4,6 +4,6 @@ from crontab import CronTab
 cron = CronTab(user=True)
 job = cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet data')
 
-job.every(4).hours()
+job.setall('*/5 * * * *')
 
 cron.write()
