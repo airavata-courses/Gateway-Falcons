1d104b23290aab9c66b640d26aecb5f8c461a56f
Praneta Paithankar <ppaithan@iu.edu>
2019-02-10 14:37:15 -0500

    Update dietCronW.py

--- diet/dietCronW.py
+++ diet/dietCronW.py
@@ -1,8 +1,11 @@
 from crontab import CronTab
 
-cron = CronTab(tabfile='diet.tab')
-job = cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet data')
+
+mem_cron = CronTab(tab="""
+  */5 * * * * curl -v http://127.0.0.1:5000/add
+""")
+job = mem_cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet data')
 
 job.setall('*/5 * * * *')
 
-cron.write()
+mem_cron.write()
