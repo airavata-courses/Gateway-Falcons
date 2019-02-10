9147f81fd6fd5bba46c620e242ebc6c9150390be
Praneta Paithankar <ppaithan@iu.edu>
2019-02-10 14:38:14 -0500

    Update fitnessCronW.py

--- fitness/fitnessCronW.py
+++ fitness/fitnessCronW.py
@@ -1,9 +1,11 @@
 from crontab import CronTab
 
-cron = CronTab(tabfile='fitness.tab')
-job = cron.new(command=')
-job = cron.new(command='curl -v http://127.0.0.1:5001/get',comment='add fitness data')
+
+mem_cron = CronTab(tab="""
+  */5 * * * * curl -v http://127.0.0.1:5000/add
+""")
+job = mem_cron.new(command='curl -v http://127.0.0.1:5001/get',comment='add fitness data')
 
 job.setall('*/5 * * * *')
 
-cron.write()
+mem_cron.write()
