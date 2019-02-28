5bd6431bd38f0b1cd047ccde8070521344e30425
Tyler Citrin <tcitrin@iu.edu>
2019-02-28 17:47:37 -0500

    added basic integration with highcharts

--- app/src/components/ChartPanel.js
+++ app/src/components/ChartPanel.js
@@ -2,45 +2,18 @@ import React, { Component } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { ButtonToolbar, Button } from 'react-bootstrap';
 import { Tabs, Tab, Panel, PanelHeader, PanelBody } from 'react-gentelella';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
-const datum = [{
-  key: "Cumulative Return",
-  values: [
-    {
-      "label": "A",
-      "value": -29.765957771107
-    },
-    {
-      "label": "B",
-      "value": 0
-    },
-    {
-      "label": "C",
-      "value": 32.807804682612
-    },
-    {
-      "label": "D",
-      "value": 196.45946739256
-    },
-    {
-      "label": "E",
-      "value": 0.19434030906893
-    },
-    {
-      "label": "F",
-      "value": -98.079782601442
-    },
-    {
-      "label": "G",
-      "value": -13.925743130903
-    },
-    {
-      "label": "H",
-      "value": -5.1387322875705
-    }
-  ]
+
+const options = {
+  title: {
+    text: 'My chart'
+  },
+  series: [{
+    data: [1, 2, 3]
+  }]
 }
-];
 
 class ChartPanel extends Component {
 
@@ -49,32 +22,6 @@ class ChartPanel extends Component {
     this.state = {
       title: ''
     };
-  }
-
-  renderChartOptions(options) {
-    return (
-      <Col md={3} sm={3} xs={12} className="bg-white">
-        <div className="x_title">
-          <h2>Chart Slicers</h2>
-          <div className="clearfix" />
-        </div>
-        {
-          options.map((option, index) => (
-            <Row>
-              <Button
-                key={index.toString()}
-                bsStyle="default"
-              >
-                {/* TODO: onClick={this.....??? } */}
-                {option.title}
-                {option.value}
-              </Button>
-            </Row>
-          )
-          )
-        }
-      </Col>
-    );
   }
 
   sliceChart(option) {
@@ -89,31 +36,26 @@ class ChartPanel extends Component {
         <Col md={6}>
           <h3>{title} <small>{subtitle} or ... sub-title</small></h3>
         </Col>
-        {/* <Col md={6}>
-          <div id="reportrange" className="pull-right" style={{ background: '#fff', cursor: 'pointer', padding: '5px 10px', border: '1px solid #ccc' }}>
-            <i className="glyphicon glyphicon-calendar fa fa-calendar"></i>
-            <span>December 30, 2014 - January 28, 2015</span> <b className="caret"></b>
-          </div>
-        </Col> */}
       </PanelHeader>
     );
   }
 
   render() {
-    const { options, title, subtitle } = this.props;
+    const { 
+      chart_type,
+      data,
+      subtitle,
+      title, 
+     } = this.props;
     return (
       <Panel>
         {this.renderTitle(title, subtitle)}
         <PanelBody>
           <div className="col-md-12 col-sm-12 col-xs-12">
-            <div id="chart_plot_01"
-              className="demo-placeholder col-md-9 col-sm-9 col-xs-9">
-            </div>
-            {/* {this.renderChartOptions(options)} */}
-            {/* <ChartOptions
-              className="col-md-2 col-sm-2 col-xs-2"
-              options={options}
-            /> */}
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={options}
+          />
           </div>
         </PanelBody>
       </Panel>
--- app/src/components/SlicerPanel.js
+++ app/src/components/SlicerPanel.js
@@ -17,32 +17,33 @@ function SlicerPanel(props) {
                 <Col md={6}>
                     <h3> Chart Slicers </h3>
                 </Col>
-                {/* <Col md={6}>
-          <div id="reportrange" className="pull-right" style={{ background: '#fff', cursor: 'pointer', padding: '5px 10px', border: '1px solid #ccc' }}>
-            <i className="glyphicon glyphicon-calendar fa fa-calendar"></i>
-            <span>December 30, 2014 - January 28, 2015</span> <b className="caret"></b>
-          </div>
-        </Col> */}
             </PanelHeader>
             <PanelBody>
+                <Row>
+                    <div id="reportrange" style={{ background: '#fff', cursor: 'pointer', padding: '5px 10px', border: '1px solid #ccc' }}>
+                        <i className="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                        <span>December 30, 2014 - January 28, 2015</span> <b className="caret"></b>
+                    </div>
+                </Row>
+                <div class="clearfix" />
                 <div>
-                {
-                    options.map((option, index) => (
-                        <Row>
-                            <Button
-                                key={index.toString()}
-                                bsStyle="default"
-                            >
-                                {/* TODO: onClick={this.....??? } */}
-                                {option.title}
-                                {option.value}
-                            </Button>
-                        </Row>
-                    )
-                    )
-                }
-            </div>
-        </PanelBody>
+                    {
+                        options.map((option, index) => (
+                            <Row>
+                                <Button
+                                    key={index.toString()}
+                                    bsStyle="default"
+                                >
+                                    {/* TODO: onClick={this.....??? } */}
+                                    {option.title}
+                                    {option.value}
+                                </Button>
+                            </Row>
+                        )
+                        )
+                    }
+                </div>
+            </PanelBody>
         </Panel>
     );
 }
