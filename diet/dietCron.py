493809cc1e4893d545e44a8ddd03f1a6db79129a
Praneta Paithankar <ppaithan@iu.edu>
2019-03-01 16:15:07 -0500

    Add config parser

--- diet/dietCron.py
+++ diet/dietCron.py
@@ -4,6 +4,6 @@ from crontab import CronTab
 cron = CronTab(user=True)
 job = cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet data')
 
-job.setall('*/5 * * * *')
+job.setall('0 08-12/4 * * *')
 
 cron.write()
