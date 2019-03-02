79860dc761e20c7ea1f65cd0c9a04a0457c2944c
Chirag Galani <chiraggalani25@gmail.com>
2019-03-02 14:05:18 -0500

    Update server Jetstream url

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,5 +1,5 @@
 // TODO::
-const serverUrl = 'http://localhost:3001';
+const serverUrl = 'http://149.165.170.57:3001/';
 const imageUrl = 'http://localhost:8081';
 // const imageUrl = 'http://localhost:8081';
 
