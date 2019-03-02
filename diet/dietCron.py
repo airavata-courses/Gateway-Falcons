819dd5edbe8e1eb799dd692adf8d4cd501afa0d8
Praneta Paithankar <praneta25@gmail.com>
2019-03-02 15:22:26 -0500

    Remove previously scheduled job
    Closes #42

--- diet/dietCron.py
+++ diet/dietCron.py
@@ -2,6 +2,9 @@ from crontab import CronTab
 
 
 cron = CronTab(user=True)
+iter1=cron.find_comment("add diet data")
+for job in iter1:
+    cron.remove(job)
 job = cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet data')
 
 job.setall('0 08-12/4 * * *')
