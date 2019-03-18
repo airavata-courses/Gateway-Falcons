8c4b5cf441de28467053730c62305ae9817f5d3b
Tyler Citrin <tcitrin@iu.edu>
2019-03-17 22:13:17 -0400

    linked cardio mood to front end all the way through

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const parseFile = require('./utils/json_fileparser');
 const mlab_credentials = parseFile(process.env.MLAB_CREDENTIALS);
 
 mongoose
-  .connect(mlab_credentials.mongoURI)
+  .connect(mlab_credentials.mongoURI, { useNewUrlParser: true })
   .then(() => console.log('mlab connected successfully'))
   .catch((err) => console.error('error connecting to mlab:', err))
 
@@ -30,27 +30,27 @@ mongoose
 //  }
 // app.use(convert(cors({ origin: checkOriginAgainstWhitelist })));
 
-app.use(cors({ origin: "http://149.165.170.161:3000"}))
+app.use(cors({ origin: "http://149.165.170.161:3000" }))
 // app.use(cors({ origin: "http://localhost:3000"}))
 
 fs.readdirSync('./routes').forEach(file => {
-    const fileName = file.substring(0, file.indexOf('.'));
-    const router = require(`./routes/${fileName}`);
-    // console.log(router);
-    if (fileName !== 'index') {
-        app.use(`/${fileName}`, router);
-    } else {
-        app.use(`/`, router);      
-    }
+  const fileName = file.substring(0, file.indexOf('.'));
+  const router = require(`./routes/${fileName}`);
+  // console.log(router);
+  if (fileName !== 'index') {
+    app.use(`/${fileName}`, router);
+  } else {
+    app.use(`/`, router);
+  }
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
