b66df2c24a91efb986efb92ceda36a9ca3650044
Praneta Paithankar <praneta25@gmail.com>
2019-03-24 15:35:27 -0400

    Update dietCron.py

--- diet/dietCron.py
+++ diet/dietCron.py
@@ -7,6 +7,6 @@ for job in iter1:
     cron.remove(job)
 job = cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet data')
 
-job.setall('0 08-12/4 * * *')
+job.setall('59 07-23/4 * * *')
 
 cron.write()
