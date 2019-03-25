a6a7ec1921a32636cb423f3c3a1c496d2613b50b
Praneta Paithankar <ppaithan@iu.edu>
2019-03-24 21:27:57 -0400

    Add weather Data

--- Location/app.py
+++ Location/app.py
@@ -5,7 +5,7 @@ import os
 from flask_cors import cross_origin
 
 from pyvirtualdisplay import Display
-
+import urllib.request, json
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -32,7 +32,7 @@ else:
 
 app.config['MONGO_DBNAME'] = parser.get('DB','dbname')
 app.config['MONGO_URI'] = parser.get('DB', 'url')
-
+token = parser.get('WEATHER','token')
 mongo = PyMongo(app)
 
 
@@ -76,47 +76,80 @@ def get_location():
         average_speed = soup.select('span[class="stat average-speed"]')[0].text
         units_avg_speed = soup.select('span[class="units units-avg-speed"]')[0].text
 
-        unit_distance = soup.select('span[class="units units-distance"]')[0].text
-        units_speed = soup.select('span[class="units units-speed"]')[0].text
-        total_climb = soup.select('span[class="stat total-climb"]')[0].text + " " + unit_distance
-        total_descent = soup.select('span[class="stat total-descent"]')[0].text + " " + unit_distance
-        max_elevation = soup.select('span[class="stat max-elevation"]')[0].text + " " + unit_distance
-        max_grade = soup.select('span[class="stat max-grade"]')[0].text + " " + "%"
-        avg_speed = soup.select('span[class="stat avg-speed"]')[0].text + " " + units_speed
-        max_speed = soup.select('span[class="stat max-speed"]')[0].text + " " + units_speed
+        total_climb = soup.select('span[class="stat total-climb"]')[0].text
+        total_descent = soup.select('span[class="stat total-descent"]')[0].text
+        max_elevation = soup.select('span[class="stat max-elevation"]')[0].text
+        max_grade = soup.select('span[class="stat max-grade"]')[0].text
+        avg_speed = soup.select('span[class="stat avg-speed"]')[0].text
+        max_speed = soup.select('span[class="stat max-speed"]')[0].text
 
-        unit_heart_rate = "bpm"
-        unit_cadence = "rpm"
-        avg_heart_rate = soup.select('span[class="stat avg-hr"]')[0].text + " " + unit_heart_rate
-        max_heart_rate = soup.select('span[class="stat max-hr"]')[0].text + " " + unit_heart_rate
-        avg_cadence = soup.select('span[class="stat avg-cadence"]')[0].text + " " + unit_cadence
+        avg_heart_rate = soup.select('span[class="stat avg-hr"]')[0].text
+        max_heart_rate = soup.select('span[class="stat max-hr"]')[0].text
+        avg_cadence = soup.select('span[class="stat avg-cadence"]')[0].text
 
-        max_cadence = soup.select('span[class="stat max-cadence"]')[0].text + " " + unit_cadence
+        max_cadence = soup.select('span[class="stat max-cadence"]')[0].text
         location_db.delete_many({
             "workout_date_time": workout_date_time
         })
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=" + data_lat + "&lon=" + data_lon + "&APPID=" + token
+        weather = "NOT_DEFINED"
+        weather_desc = "NOT_DEFINED"
+        temperature = "NOT_DEFINED"
+        pressure = "NOT_DEFINED"
+        humidity = "NOT_DEFINED"
+        visibility = "NOT_DEFINED"
+        sea_level = "NOT_DEFINED"
+        grnd_level = "NOT_DEFINED"
+
+        with urllib.request.urlopen(url) as url_loaded:
+            try:
+                data = json.loads(url_loaded.read().decode())
+                weather = data["weather"][0]["main"]
+                weather_desc = data["weather"][0]["description"]
+                temperature = data["main"]["temp"]
+                pressure = data["main"]["pressure"]
+                humidity = data["main"]["humidity"]
+                wind_speed = data["wind"]["speed"]
+                wind_deg = data["wind"]["deg"]
+                visibility = data["visibility"]
+                sea_level = data["main"]["sea_level"]
+                grnd_level = data["main"]["grnd_level"]
+
+            except KeyError:
+                pass
 
         location_db.insert_one({
             "workout_date_time": workout_date_time,
-            "data-live" : data_live,
-            "data-lon" : data_lon,
-            "data-mb-url" : data_mb_url,
-            "workout_status" : workout_status,
-            "elapsed-time" : elapsed_time,
-            "total_distance" : total_distance ,
-            "unit_total_distance" : unit_total_distance,
-            "average_speed" : average_speed ,
-            "units_average_speed" : units_avg_speed,
-            "total_climb" : total_climb,
-            "total_descent" : total_descent,
-            "max-elevation" : max_elevation,
-            "max_grade" : max_grade,
-            "avg_speed" : avg_speed,
-            "max_speed" : max_speed,
-            "avg_heart_rate" : avg_heart_rate,
-            "max_heart_rate" : max_heart_rate,
-            "avg_cadence" : avg_cadence,
-            "max_cadence" : max_cadence
+            "data_live": data_live,
+            "data_lon": float(data_lon),
+            "data_lat": float(data_lat),
+            "data_mb_url": data_mb_url,
+            "workout_status": workout_status,
+            "elapsed_time": elapsed_time,
+            "total_distance": total_distance,
+            "unit_total_distance": unit_total_distance,
+            "average_speed": average_speed,
+            "units_average_speed": units_avg_speed,
+            "total_climb": total_climb,
+            "total_descent": total_descent,
+            "max_elevation": max_elevation,
+            "max_grade": max_grade,
+            "avg_speed": avg_speed,
+            "max_speed": max_speed,
+            "avg_heart_rate": avg_heart_rate,
+            "max_heart_rate": max_heart_rate,
+            "avg_cadence": avg_cadence,
+            "max_cadence": max_cadence,
+            "weather": weather,
+            "weather_desc": weather_desc,
+            "temperature": temperature,
+            "pressure": pressure,
+            "humidity": humidity,
+            "visibility": visibility,
+            "wind_speed": wind_speed,
+            "wind_deg": wind_deg,
+            "sea_level": sea_level,
+            "grnd_level": grnd_level
         })
 
         browser.quit()  # only use this when done with automation
