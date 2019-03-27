af010187a7a55d0a190b1f3de04f31201df8daac
Praneta Paithankar <praneta25@gmail.com>
2019-03-27 00:03:19 -0400

    Update dietCron.py

--- diet/dietCron.py
+++ diet/dietCron.py
@@ -11,13 +11,3 @@ job = cron.new(command='curl -v http://127.0.0.1:5000/add',comment='add diet dat
 job.setall('59 07-23/4 * * *')
 
 cron.write()
-
-iter1=cron.find_comment("add sleep data")
-for job in iter1:
-    cron.remove(job)
-
-job = cron.new(command='curl -v http://127.0.0.1:5000/getsleep',comment='add sleep data')
-
-job.setall('0 20 * * *')
-
-cron.write()
