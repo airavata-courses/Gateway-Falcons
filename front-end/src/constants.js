11a38624f9f3d8ff3422fa5b1c1466548aaf64ca
Tyler Citrin <tcitrin@indiana.edu>
2019-03-29 13:38:15 -0400

    new docker url

--- front-end/src/constants.js
+++ front-end/src/constants.js
@@ -1,5 +1,6 @@
 // TODO::
-const serverUrl = 'http://149.165.170.57:3001';
+// const serverUrl = 'http://149.165.170.57:3001';
+const serverUrl = 'http://149.165.168.185:30032';
 const _devServerUrl = 'http://localhost:3001';
 const imageUrl = 'http://localhost:8081';
 const zookeeperurl = 'http://149.165.157.75:5000';
