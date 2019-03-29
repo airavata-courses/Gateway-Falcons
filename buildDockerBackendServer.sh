88a7b4230899afa97436e93fc44e2ed411b510dc
Chirag Galani <chiraggalani25@gmail.com>
2019-03-29 12:31:52 -0400

    Docker Script

---
+++ buildDockerBackendServer.sh
@@ -0,0 +1,14 @@
+kill -9 $(lsof -i:3001 -t) || echo $?
+
+cd ./server
+rm -rf .env
+rm -rf config/
+cp ../../.env ./
+cp -r ../../config ./
+npm install
+
+cd ..
+# docker login
+docker build -f DockerFileBackendServer -t chiraggalani25/backend_server .
+docker run -p 3001:3001 -d chiraggalani25/backend_server
+#docker push chiraggalani25/backend_server
