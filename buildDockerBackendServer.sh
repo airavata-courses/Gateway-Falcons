1e82f4b950046981d49e869c66bf1bbe64e6f06b
Praneta Paithankar <praneta25@gmail.com>
2019-03-30 18:41:29 -0400

    Update buildDockerBackendServer.sh

--- buildDockerBackendServer.sh
+++ buildDockerBackendServer.sh
@@ -8,7 +8,7 @@ cp -r ../../config ./
 npm install
 
 cd ..
-# docker login
-docker build -f DockerFileBackendServer -t chiraggalani25/backend_server .
+docker login
+docker build -f DockerFileBackendServer -t johnlive/backend_server .
 #docker run -p 3001:3001 -d chiraggalani25/backend_server
-docker push chiraggalani25/backend_server
+docker push johnlive/backend_server
