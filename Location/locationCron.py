f91fa016ba0b96a9e81762680ff07354e901cb58
Praneta Paithankar <praneta25@gmail.com>
2019-03-30 15:25:58 -0400

    Update locationCron.py

--- Location/locationCron.py
+++ Location/locationCron.py
@@ -5,7 +5,7 @@ cron = CronTab(user=True)
 iter1=cron.find_comment("add location data")
 for job in iter1:
     cron.remove(job)
-job = cron.new(command='curl -v http://127.0.0.1:5000/getlocation',comment='add location data')
+job = cron.new(command='curl -v http://127.0.0.1:5006/getlocation',comment='add location data')
 
 job.setall('*/1 * * * *')
 
