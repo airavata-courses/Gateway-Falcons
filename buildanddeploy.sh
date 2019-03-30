a3881b0d98aba7eaa8ff8058fcfdad042fe1879a
Praneta Paithankar <praneta25@gmail.com>
2019-03-30 18:56:02 -0400

    Create buildanddeploy.sh

---
+++ buildanddeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf diet/
+mkdir diet
+cd diet
+git clone https://github.com/airavata-courses/Gateway-Falcons
+cd ./Gateway-Falcons
+git checkout diet-service
+cp ../../dietconfig.ini diet/dietconfig.ini
+mkdir scripts
+cp ~/scripts/*.sh ~/diet/Gateway-Falcons/scripts
+docker build --tag=$1 .
+docker login -u johnlive -p johnschwenck
+docker tag $1 johnlive/$2
+docker push johnlive/$2
