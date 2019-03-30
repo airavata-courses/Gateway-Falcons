9fccf8ce22ef09cdc9bb823d09e57c495b2e9972
Praneta Paithankar <praneta25@gmail.com>
2019-03-30 14:59:01 -0400

    Update app.py

--- Location/app.py
+++ Location/app.py
@@ -3,7 +3,6 @@ from configparser import ConfigParser
 from flask_pymongo import PyMongo
 import os
 from flask_cors import cross_origin
-
 from pyvirtualdisplay import Display
 import urllib.request, json
 from selenium import webdriver
@@ -21,6 +20,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from xvfbwrapper import Xvfb
+from datetime import datetime
 
 parser = ConfigParser()
 app = Flask(__name__)
@@ -35,24 +37,20 @@ app.config['MONGO_URI'] = parser.get('DB', 'url')
 token = parser.get('WEATHER','token')
 mongo = PyMongo(app)
 
-
 @app.route('/getlocation', methods=['POST','GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def get_location():
+    success=False
     display = Display(visible=0, size=(800, 600))
     display.start()
     baseurl = 'https://www.wahooligan.com/users/live/Am0zG9KkP9S7qnhA6IU8eQ'
-    options = Options()
-    options.add_argument('-headless')
+   
     cap = DesiredCapabilities().FIREFOX
     cap['marionette'] = False
-
-    browser = webdriver.Firefox(capabilities=cap)
-
+    browser = webdriver.Firefox(capabilities=cap,executable_path='/usr/bin/geckodriver')
     browser.get(baseurl)
-    delay = 10  # seconds
+    delay = 20  # seconds
     location_db = mongo.db.location
-
     try:
         myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'livetrack-map')))
         soup = BeautifulSoup(browser.page_source, features="html.parser")
@@ -62,7 +60,6 @@ def get_location():
         data_lon = x[0].attrs["data-lon"]
         data_mb_url = x[0].attrs["data-mb-url"]
         status = soup.select('div[class="livetrack"]')
-        # print("length= ", len(soup.select('span[class="workout-status"]')))
         workout_status_arr = workout_status = soup.select('div[class="workout-status"]')
         if len(workout_status_arr) > 0:
             workout_status = workout_status_arr[0].text
@@ -75,31 +72,26 @@ def get_location():
         unit_total_distance = soup.select('span[class="units units-total-distance"]')[0].text
         average_speed = soup.select('span[class="stat average-speed"]')[0].text
         units_avg_speed = soup.select('span[class="units units-avg-speed"]')[0].text
-
         total_climb = soup.select('span[class="stat total-climb"]')[0].text
         total_descent = soup.select('span[class="stat total-descent"]')[0].text
         max_elevation = soup.select('span[class="stat max-elevation"]')[0].text
         max_grade = soup.select('span[class="stat max-grade"]')[0].text
         avg_speed = soup.select('span[class="stat avg-speed"]')[0].text
         max_speed = soup.select('span[class="stat max-speed"]')[0].text
-
         avg_heart_rate = soup.select('span[class="stat avg-hr"]')[0].text
         max_heart_rate = soup.select('span[class="stat max-hr"]')[0].text
         avg_cadence = soup.select('span[class="stat avg-cadence"]')[0].text
-
         max_cadence = soup.select('span[class="stat max-cadence"]')[0].text
-        location_db.delete_many({
-            "workout_date_time": workout_date_time
-        })
+
         url = "https://api.openweathermap.org/data/2.5/weather?lat=" + data_lat + "&lon=" + data_lon + "&APPID=" + token
         weather = "NOT_DEFINED"
         weather_desc = "NOT_DEFINED"
-        temperature = "NOT_DEFINED"
-        pressure = "NOT_DEFINED"
-        humidity = "NOT_DEFINED"
-        visibility = "NOT_DEFINED"
-        sea_level = "NOT_DEFINED"
-        grnd_level = "NOT_DEFINED"
+        temperature = -1
+        pressure = -1
+        humidity = -1
+        visibility = -1
+        sea_level = -1
+        grnd_level = -1
 
         with urllib.request.urlopen(url) as url_loaded:
             try:
@@ -114,11 +106,15 @@ def get_location():
                 visibility = data["visibility"]
                 sea_level = data["main"]["sea_level"]
                 grnd_level = data["main"]["grnd_level"]
+            except Exception as e:
+                print(e)
+        
+        location_db.delete_many({
+            "workout_date_time": workout_date_time
+        })
 
-            except KeyError:
-                pass
-
-        location_db.insert_one({
+        location_db.insert_one(
+        {
             "workout_date_time": workout_date_time,
             "data_live": data_live,
             "data_lon": float(data_lon),
@@ -126,39 +122,44 @@ def get_location():
             "data_mb_url": data_mb_url,
             "workout_status": workout_status,
             "elapsed_time": elapsed_time,
-            "total_distance": total_distance,
+            "total_distance": float(total_distance),
             "unit_total_distance": unit_total_distance,
-            "average_speed": average_speed,
+            "average_speed": float(average_speed),
             "units_average_speed": units_avg_speed,
-            "total_climb": total_climb,
-            "total_descent": total_descent,
-            "max_elevation": max_elevation,
-            "max_grade": max_grade,
-            "avg_speed": avg_speed,
-            "max_speed": max_speed,
-            "avg_heart_rate": avg_heart_rate,
-            "max_heart_rate": max_heart_rate,
-            "avg_cadence": avg_cadence,
-            "max_cadence": max_cadence,
+            "total_climb": float(total_climb),
+            "total_descent": float(total_descent),
+            "max_elevation": float(max_elevation),
+            "max_grade": float(max_grade),
+            "avg_speed": float(avg_speed),
+            "max_speed": float(max_speed),
+            "avg_heart_rate": float(avg_heart_rate),
+            "max_heart_rate": float(max_heart_rate),
+            "avg_cadence": float(avg_cadence),
+            "max_cadence": float(max_cadence),
             "weather": weather,
             "weather_desc": weather_desc,
-            "temperature": temperature,
-            "pressure": pressure,
-            "humidity": humidity,
-            "visibility": visibility,
-            "wind_speed": wind_speed,
-            "wind_deg": wind_deg,
-            "sea_level": sea_level,
-            "grnd_level": grnd_level
+            "temperature": float(temperature),
+            "pressure": float(pressure),
+            "humidity": float(humidity),
+            "visibility": float(visibility),
+            "wind_speed": float(wind_speed),
+            "wind_deg": float(wind_deg),
+            "sea_level": float(sea_level),
+            "grnd_level": float(grnd_level),
+            "time_added": str(datetime.now())
         })
-
-        browser.quit()  # only use this when done with automation
-        display.stop()
-
+        success=True
     except TimeoutException:
         print("Loading took too much time!")
-    return 'Location is added.'
-
+    except Exception as e: 
+        print(e)
+    finally:
+        browser.quit()  # only use this when done with automation
+        display.stop()
+        if(success):
+            return('Location is added.')
+        else:
+            return('Location is NOT added.')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
