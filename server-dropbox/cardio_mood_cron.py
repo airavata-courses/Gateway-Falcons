410f363eca5bb2e3d44a7636800de76c6063e302
Praneta Paithankar <praneta25@gmail.com>
2019-03-30 13:09:58 -0400

    Update cardio_mood_cron.py

--- server-dropbox/cardio_mood_cron.py
+++ server-dropbox/cardio_mood_cron.py
@@ -5,7 +5,7 @@ cron = CronTab(user=True)
 iter1=cron.find_comment("add location data")
 for job in iter1:
     cron.remove(job)
-job = cron.new(command='curl -v http://127.0.0.1:3003/cardio_mood',comment='add cardio data')
+job = cron.new(command='curl -v http://149.165.168.185:30092/cardio_mood/cardio_mood',comment='add cardio data')
 
 job.setall('*/1 * * * *')
 
