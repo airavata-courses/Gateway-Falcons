757503e0eb1ac26407d4ee67baedbb01cb68487f
Chirag Galani <chiraggalani25@gmail.com>
2019-03-31 15:28:50 -0400

    Update buildDockerBackendDataMicroservice.sh

--- buildDockerBackendDataMicroservice.sh
+++ buildDockerBackendDataMicroservice.sh
@@ -8,7 +8,7 @@ cp -r ../../config ./
 
 
 cd ..
-docker login
+#docker login
 docker build -f DockerFileBackendDataMicroservice -t johnlive/backend_data_microservice .
 #docker run -p 3002:3002 -d chiraggalani25/backend_data_microservice
 
