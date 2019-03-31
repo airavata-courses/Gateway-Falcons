b81f75131fb854ab5413197e10a9cc53cfd5f911
Chirag Galani <chiraggalani25@gmail.com>
2019-03-31 15:31:41 -0400

    Fix Backend dropbox script

--- buildDockerBackendDropbox.sh
+++ buildDockerBackendDropbox.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd ./Gateway-Falcons/server-dropbox
+cd server-dropbox
 rm -rf .env
 rm -rf config/
 cp ../../.env ./
 cp -r ../../config ./
 
 cd ..
-docker login
+
 docker build -f DockerFileBackendDropbox -t johnlive/backend_dropbox .
 #docker run -p 3003:3003 -d chiraggalani25/backend_dropbox
 
