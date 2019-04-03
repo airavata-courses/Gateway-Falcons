17166eab4a4d12b8b91d17c8472ff0939fb11ff0
Praneta Paithankar <praneta25@gmail.com>
2019-04-03 12:21:14 -0400

    Update cardio_mood_cron.py

--- server-dropbox/cardio_mood_cron.py
+++ server-dropbox/cardio_mood_cron.py
@@ -1,12 +1,19 @@
 from crontab import CronTab
 
-
 cron = CronTab(user=True)
-iter1=cron.find_comment("add location data")
+
+iter1=cron.find_comment("add cardio data")
 for job in iter1:
     cron.remove(job)
-job = cron.new(command='curl -v http://149.165.168.185:30092/cardio_mood/cardio_mood',comment='add cardio data')
-
-job.setall('*/1 * * * *')
-
+job = cron.new(command='curl -v http://149.165.168.185:30092/cardio_mood',comment='add cardio data')
+job.setall('59 11 * * *')
 cron.write()
+
+iter1=cron.find_comment("add blood pressure data")
+for job in iter1:
+    cron.remove(job)
+job = cron.new(command='curl -v http://149.165.168.185:30092/blood_pressure',comment='add blood pressure data')
+job.setall('59 11 * * *')
+cron.write()
+
+
