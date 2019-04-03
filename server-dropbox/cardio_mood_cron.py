e8f43d480018871b713302dc8d1e33355244f9a3
Praneta Paithankar <praneta25@gmail.com>
2019-04-03 12:31:35 -0400

    Update cardio_mood_cron.py

--- server-dropbox/cardio_mood_cron.py
+++ server-dropbox/cardio_mood_cron.py
@@ -6,14 +6,14 @@ iter1=cron.find_comment("add cardio data")
 for job in iter1:
     cron.remove(job)
 job = cron.new(command='curl -v http://149.165.168.185:30092/cardio_mood',comment='add cardio data')
-job.setall('59 11 * * *')
+job.setall('0 7 * * *')
 cron.write()
 
 iter1=cron.find_comment("add blood pressure data")
 for job in iter1:
     cron.remove(job)
 job = cron.new(command='curl -v http://149.165.168.185:30092/blood_pressure',comment='add blood pressure data')
-job.setall('59 11 * * *')
+job.setall('0 7 * * *')
 cron.write()
 
 
