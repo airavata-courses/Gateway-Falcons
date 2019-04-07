87e0c485de94d426a25fb23c8c060f50ebbb2d27
Tyler Citrin <tcitrin@iu.edu>
2019-04-07 18:27:29 -0400

    holding on to old IP

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,7 @@
-// const serverUrl = '45.55.104.93:30072';
-const serverUrl = 'localhost:30072';
+
+const serverUrl = '45.55.104.93:30072';
+// const serverUrl = 'localhost:30072';
+// const serverUrl = 'localhost:3002';
 
 module.exports = {
     serverUrl
