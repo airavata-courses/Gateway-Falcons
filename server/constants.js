614f2e610d328c8e3231b2acb78bcbc64406b91e
Chirag Galani <chiraggalani25@gmail.com>
2019-04-08 03:27:13 -0400

    Utilizing KubeDNS to access backend-data microservice

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,6 @@
 
-const serverUrl = '45.55.104.93:30072';
+// const serverUrl = '45.55.104.93:30072';
+const serverUrl = 'http://backend-data-microservice.default.svc.cluster.local:3002';
 // const serverUrl = 'localhost:30072';
 // const serverUrl = 'localhost:3002';
 
