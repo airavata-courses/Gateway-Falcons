e4700cde7d929f87e86f4c014d4bcef5799883dd
Chirag Galani <chiraggalani25@gmail.com>
2019-04-08 04:35:16 -0400

    Attempt 2 to use kubeDNS mapping

--- front-end/src/constants.js
+++ front-end/src/constants.js
@@ -2,7 +2,8 @@
 // const serverUrl = 'http://149.165.168.185:30032';
 // TODO::
 // const serverUrl = 'http://45.55.104.93:30032';
-const serverUrl = 'http://backend.default.svc.cluster.local:3001';
+// const serverUrl = 'http://backend.default.svc.cluster.local:3001';
+const serverUrl = 'backend:3001';
 // const serverUrl = 'http://localhost:3001';
 
 const _devServerUrl = 'http://localhost:3001';
