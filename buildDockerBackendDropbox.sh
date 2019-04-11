18724316e0bea2e2eaa9f140335511c4dba49ec3
Praneta Paithankar <ppaithan@iu.edu>
2019-04-11 13:53:48 -0400

    Update buildDockerBackendDropbox.sh

--- buildDockerBackendDropbox.sh
+++ buildDockerBackendDropbox.sh
@@ -3,10 +3,10 @@
 cd server-dropbox
 rm -rf .env
 rm -rf config/
-#cp ../../.env ./
-#cp -r ../../config ./
+cp ../../.env ./
+cp -r ../../config ./
 
-#cd ..
+cd ..
 
 docker build -f DockerFileBackendDropbox -t johnlive/backend_dropbox .
 #docker run -p 3003:3003 -d johnlive/backend_dropbox
