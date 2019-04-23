0876cb2c2431ab36a613ccb8c2d4b31e031228c8
Praneta Paithankar <praneta25@gmail.com>
2019-04-23 17:01:13 -0400

    Update buildDockerBackendDataMicroservice.sh

--- buildDockerBackendDataMicroservice.sh
+++ buildDockerBackendDataMicroservice.sh
@@ -9,7 +9,7 @@ cp -r ../../config ./
 
 cd ..
 #docker login
-docker build -f DockerFileBackendDataMicroservice -t johnlive/backend_data_microservice .
+docker build -f DockerFileBackendDataMicroservice -t johnlive/backend_data_microservice_blue .
 #docker run -p 3002:3002 -d chiraggalani25/backend_data_microservice
 
-docker push johnlive/backend_data_microservice
+docker push johnlive/backend_data_microservice_blue
