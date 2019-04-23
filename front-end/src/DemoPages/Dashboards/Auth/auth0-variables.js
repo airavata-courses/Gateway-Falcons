567fc4289860aeb3397dc50afe240b2cdb0d794d
Praneta Paithankar <ppaithan@iu.edu>
2019-04-22 23:34:33 -0400

    Update auth0-variables.js

--- front-end/src/DemoPages/Dashboards/Auth/auth0-variables.js
+++ front-end/src/DemoPages/Dashboards/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'pranetaads.auth0.com',
   clientId: 'ye5qM42bd2b0VTeVbzaspBfBxrs1CZwC',
-  // callbackUrl: 'http://149.165.168.185/#/landing'
-  callbackUrl: 'http://localhost:3000/dashboards/callback'
+  callbackUrl: 'http://149.165.168.185/dashboards/callback'
+  //callbackUrl: 'http://localhost:3000/dashboards/callback'
 }
