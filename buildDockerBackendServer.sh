379ca1dd0c83304a3ccced26d3478a0d8b696c47
Praneta Paithankar <praneta25@gmail.com>
2019-04-23 17:03:10 -0400

    Update buildDockerBackendServer.sh

--- buildDockerBackendServer.sh
+++ buildDockerBackendServer.sh
@@ -9,6 +9,6 @@ cp -r ../../config ./
 
 cd ..
 
-docker build -f DockerFileBackendServer -t johnlive/backend_server .
+docker build -f DockerFileBackendServer -t johnlive/backend_server_green .
 #docker run -p 3001:3001 -d chiraggalani25/backend_server
-docker push johnlive/backend_server
+docker push johnlive/backend_server_green
