a3c4b0b0af42968fc4c41a0c8c4a5e94f0979d6c
Chirag Galani <chiraggalani25@gmail.com>
2019-04-28 19:44:30 -0400

    Added test cases

--- server/test/eeg.test.js
+++ server/test/eeg.test.js
@@ -1,20 +1,18 @@
 const request = require('supertest');
-var test = require('tape');
+var expect = require('chai').expect;
 const express = require('express');
 
-describe('request(url)', function () {
 
-  const app = express();
-
-  test('should have image data', function (t) {
-
-    request(app)
-      .get('/images')
+describe('testEEGURL()', function () {
+  it('should return EEG data', function () {
+    
+    // 1. ARRANGE
+    request(express)
+      .get('/eeg')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function (err, res) {
         t.end();
       });
   })
-
 });
