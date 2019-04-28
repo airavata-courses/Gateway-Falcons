1ae8eea6bc5f9422dd6e017469f3728b2cc8af08
Praneta Paithankar <praneta25@gmail.com>
2019-04-28 12:46:45 -0400

    Create test_fitbit_app.py

---
+++ fitbit/test_fitbit_app.py
@@ -0,0 +1,18 @@
+import mock
+import fitbitapp
+
+test_app = fitbitapp.app
+test_client = test_app.test_client()
+test_app.testing = True
+
+def test_sleep_data():
+    response = test_client.get('/getsleep')
+    assert b'Sleep data added [Green DEPLOYMENT]' in response.data
+
+def test_hr_data():
+    response = test_client.get('/getheartrate')
+    assert b'Heart rate added [Green DEPLOYMENT]' in response.data
+
+def test_stat_data():
+    response = test_client.get('/getstat')
+    assert b'Series data added [Green DEPLOYMENT]' in response.data
