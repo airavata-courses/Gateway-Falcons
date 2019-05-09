7cc9b6d45d1044c5c5aa46de72a0d4471a35dd4e
Praneta Paithankar <ppaithan@iu.edu>
2019-05-09 17:49:27 -0400

    Update dietCron.py

--- diet/dietCron.py
+++ diet/dietCron.py
@@ -6,7 +6,7 @@ iter1=cron.find_comment("add diet data")
 for job in iter1:
     cron.remove(job)
 
-job = cron.new(command='curl -v http://149.165.168.185:30082/add',comment='add diet data')
+job = cron.new(command='curl -v http://45.55.104.93:30082/add',comment='add diet data')
 
 job.setall('59 07-23/4 * * *')
 
